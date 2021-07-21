2fcf87ab91eff8655f193435f6c4f09d5cdab4ab
Your Name <you@example.com>
2021-07-21 15:07:36 +0300

    updated registration

--- RiotMap/settings.py
+++ RiotMap/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+_f@dcdtdkx+xi^%)_z2ga$5=$u5pc$9aq$@p*1aro870&dx54'
+SECRET_KEY = '1234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
