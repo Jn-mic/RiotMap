59efc689875d6aeb616fa08412da153c5ec0bfe9
Your Name <you@example.com>
2021-07-21 14:51:52 +0300

    defined the registration/login templates,functions and views

---
+++ authy/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def register(request):
+    context = {}
+    return render(request, 'register.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'login.html', context)
