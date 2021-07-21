a871001a81908dfd994eb073e05f79068c57f2c0
Your Name <you@example.com>
2021-07-21 19:03:33 +0300

    authentication in working state

--- RiotMap/settings.py
+++ RiotMap/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'authy',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
